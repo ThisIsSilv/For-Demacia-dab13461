c9091a1c7671685b869a8ed644840df3ad774168
Silvano Bakacak <2119823@talnet.nl>
2019-10-09 13:24:43 +0200

    Uitwerking van de exercise

---
+++ for_klas.php
@@ -0,0 +1,16 @@
+<?php
+$array = array(
+     "Kreeft\n",
+     "Bob\n",
+     "Klaas\n",
+     "Man\n",
+     "Geel\n",
+     "Driehoek\n",
+     "kwart voor drie\n",
+
+
+);
+for ($i = 0; count ($array); $i++) {
+    echo $array[$i];
+}
+
